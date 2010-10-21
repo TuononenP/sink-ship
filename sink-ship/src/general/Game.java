2cba7c720862a0790f02a25aa6f8cbc3a180820b
petri.tuononen <petri.tuononen@b06aadea-e8ee-e0b9-abe3-2a3b5def36ea>
2010-10-21 07:09:46 +0000

    Moved socket related classes to a socket package.

--- sink-ship/src/general/Game.java
+++ sink-ship/src/general/Game.java
@@ -16,6 +16,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package general;
 
+import socket.SocketListenerWorker;
+
 /**
  * @author Petri Tuononen
  * @author Matej Pristak
