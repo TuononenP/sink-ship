026f96b5a835d829d384e0a8ca09f7f1458046e0
petri.tuononen <petri.tuononen@b06aadea-e8ee-e0b9-abe3-2a3b5def36ea>
2010-11-10 00:51:08 +0000

    Gui is now centered on the screen. It takes into an account the end-users monitor resolution and calculates the center placement from the size of the frame.

--- trunk/sink-ship/src/gui/windows/BoardTest.java
+++ trunk/sink-ship/src/gui/windows/BoardTest.java
@@ -1,3 +1,19 @@
+/*
+Copyright (C) 2010 Petri Tuononen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package gui.windows;
 
 import java.awt.EventQueue;
@@ -9,17 +25,23 @@ import gui.Gui;
 import gui.panels.BoardPanel;
 
 //should show the board
+/**
+ * @author Petri Tuononen
+ */
 public class BoardTest extends Gui {
 
+	private static final long serialVersionUID = 6307653912816584540L;
+	
+	private static LayoutManager layout;
+	
 	public BoardTest(Dimension d, LayoutManager layout) {
 		super(new Dimension(500, 500), new BorderLayout());
-		getFrame().add(new BoardPanel());
+		BoardTest.layout=layout;
+		add(new BoardPanel());
 	}
 
-	private static final long serialVersionUID = -1853410790696067722L;
-
 	public static void main(String[] args) {
-		EventQueue.invokeLater(getGui());
+		EventQueue.invokeLater(new BoardTest(getDimension(), layout));
 	}
 
 }
